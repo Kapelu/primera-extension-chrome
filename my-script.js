95d268c38144f12b353e2001bba7ed1046129722
kapelu <ddaniel_calderon@hotmail.com>
2024-01-12 23:33:32 -0300

    Update 12-01-2024

--- my-script.js
+++ my-script.js
@@ -1,7 +1,7 @@
-const mensaje = document.querySelector("div");
-const boton = document.getElementById("cambiar-mensaje");
+const mensaje = document.querySelector('div')
+const boton = document.getElementById('cambiar-mensaje')
 
-boton.addEventListener("click", () => {
-  mensaje.textContent = "Nuevo mensaje";
-  alert("works")
-});
+boton.addEventListener('click', () => {
+	mensaje.textContent = 'Nuevo mensaje'
+	alert('works')
+})
